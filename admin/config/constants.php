1929fe2b7a6dd2fa58444e2b01598a0a921de4b4
Gina Rodolfo <110985809+STINGRAY008@users.noreply.github.com>
2022-12-10 07:31:15 +0800

    Set constants.php to use localhost

--- admin/config/constants.php
+++ admin/config/constants.php
@@ -14,20 +14,20 @@
 
 
 
-    /* //Creste Constants to Store Non Repeating Values
+    //Creste Constants to Store Non Repeating Values
     define('SITEURL', 'http://localhost:8080/Superlove-Food-Order/');
     //define('SITEURL', 'https://superlove-food-order.000webhostapp.com/');
 
     define('LOCALHOST', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'superlove-food-order'); */
+    define('DB_NAME', 'superlove-food-order');
 
-    define('SITEURL', 'us-cdbr-east-06.cleardb.net/index.php');
+    /* define('SITEURL', 'us-cdbr-east-06.cleardb.net/index.php');
     define('LOCALHOST', 'us-cdbr-east-06.cleardb.net');
     define('DB_USERNAME', 'ba73ee959e105c');
     define('DB_PASSWORD', 'd7bad640');
-    define('DB_NAME', 'heroku_773fcef69bc6fe9 '); 
+    define('DB_NAME', 'heroku_773fcef69bc6fe9 ');  */
 
   /*   mysql://ba73ee959e105c:d7bad640@us-cdbr-east-06.cleardb.net/heroku_773fcef69bc6fe9?reconnect=true
 
@@ -37,13 +37,13 @@
 
         DB_name: heroku_773fcef69bc6fe9 */
 
-        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        /* $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
         $cleardb_server = $cleardb_url["host"];
         $cleardb_username = $cleardb_url["user"];
         $cleardb_password = $cleardb_url["pass"];
         $cleardb_db = substr($cleardb_url["path"],1);
         $active_group = 'default';
-        $query_builder = TRUE;
+        $query_builder = TRUE; */
  
 
 
