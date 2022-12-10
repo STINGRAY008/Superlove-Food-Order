94b3db3572e5df3620a9e1d3f20c9acbdd29a268
Gina Rodolfo <110985809+STINGRAY008@users.noreply.github.com>
2022-12-10 08:58:36 +0800

    new set up

--- admin/config/constants.php
+++ admin/config/constants.php
@@ -2,19 +2,20 @@
     //Start Session
     session_start();
 
-    /* //Creste Constants to Store Non Repeating Values
-    //define('SITEURL', 'http://localhost:8080/Superlove-Food-Order/');
-    define('SITEURL', 'https://superlove-food-order.000webhostapp.com/');
+     //Create Constants to Store Non Repeating Values
+     define('SITEURL', 'http://localhost:8080/Superlove-Food-Order/');
+     define('LOCALHOST', 'localhost');
+     define('DB_USERNAME', 'root');
+     define('DB_PASSWORD', '');
+     define('DB_NAME', 'food-order');
+     
+     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
+     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //SElecting Database
 
-    define('LOCALHOST', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'superlove-food-order'); */
+     //$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db); 
 
 
-
-
-    //Creste Constants to Store Non Repeating Values
+  /*  //Creste Constants to Store Non Repeating Values
     define('SITEURL', 'http://localhost:8080/Superlove-Food-Order/');
     //define('SITEURL', 'https://superlove-food-order.000webhostapp.com/');
 
@@ -22,6 +23,24 @@
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
     define('DB_NAME', 'superlove-food-order');
+
+
+$db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //Selecting Database
+
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db); */
+
+
+
+
+    /* //Creste Constants to Store Non Repeating Values
+    define('SITEURL', 'http://localhost:8080/Superlove-Food-Order/');
+    //define('SITEURL', 'https://superlove-food-order.000webhostapp.com/');
+
+    define('LOCALHOST', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'superlove-food-order');  */
 
     /* define('SITEURL', 'us-cdbr-east-06.cleardb.net/index.php');
     define('LOCALHOST', 'us-cdbr-east-06.cleardb.net');
@@ -44,16 +63,5 @@
         $cleardb_db = substr($cleardb_url["path"],1);
         $active_group = 'default';
         $query_builder = TRUE; */
- 
-
-
-//$conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
-   //$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
-$db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //Selecting Database
-
-  // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
 
-?>
